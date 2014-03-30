040bf4c2f0dc03f430a2f4647f42c5e8079f80e5
lensky <lensky84@gmail.com>
2014-03-30 20:35:31 +0300

    fix migration

--- app/DoctrineMigrations/Version20140330195306.php
+++ app/DoctrineMigrations/Version20140330195306.php
@@ -16,6 +16,11 @@ class Version20140330195306 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
         $this->addSql("ALTER TABLE sale ADD slug VARCHAR(128) NOT NULL");
+        $stat = $this->connection->prepare('SELECT * FROM sale');
+        $stat->execute();
+        while ($row = $stat->fetch()) {
+            $this->addSql("UPDATE sale SET slug = '" . uniqid() . "'");
+        }
         $this->addSql("CREATE UNIQUE INDEX UNIQ_E54BC005989D9B62 ON sale (slug)");
     }
 
